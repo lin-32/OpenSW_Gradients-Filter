1fc1222adb897383e56eaf39f7185221123a5763
anjijj <113444930+anjijj@users.noreply.github.com>
2022-12-01 18:09:20 +0900

    Update code.py
    Among Laplacian filters, a function through Sobel boundary detection function was added to the code.

--- code.py
+++ code.py
@@ -9,6 +9,8 @@ img = cv2.imread('lion.jpg')
 
 
 laplacian = cv2.Laplacian(img, cv2.CV_8U)
+sobelx = cv2.Sobel(img,cv2.CV_8U,1,0)
+sobely = cv2.Sobel(img,cv2.CV_8U,0,1)
 
 plt.figure(figsize=(15, 8))
 
@@ -20,6 +22,16 @@ plt.axis("off")
 plt.subplot(2, 2, 2)
 plt.imshow(laplacian, cmap='gray')
 plt.title('Laplacian')
+plt.axis("off")
+       
+plt.subplot(2, 2, 3)
+plt.imshow(sobelx, cmap='gray')
+plt.title('Sobel X')
+plt.axis("off")
+
+plt.subplot(2, 2, 4)
+plt.imshow(sobely, cmap='gray')
+plt.title('Sobel Y')
 plt.axis("off")
 
 plt.tight_layout()
