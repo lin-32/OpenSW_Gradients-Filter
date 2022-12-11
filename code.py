b5b9e4d33f32294de867da0bf8f41e85430fa8a0
Jeong Hae Jun <105699546+nujeah@users.noreply.github.com>
2022-12-11 12:40:49 +0900

    Update the Code
    Corrected some error-causing code to ensure correct output.

--- code.py
+++ code.py
@@ -1,46 +1,45 @@
 import matplotlib.pyplot as plt
 import cv2
-img = cv2.imread('./img/lion.jpg')
+img = cv2.imread('lion.jpeg')
 
 laplacian = cv2.Laplacian(img, cv2.CV_8U)
 sobelx = cv2.Sobel(img,cv2.CV_8U,1,0)
 sobely = cv2.Sobel(img,cv2.CV_8U,0,1)
-blur2 = cv2.medianBlur(img, cv2.CV_8U, 3)
+blur2 = cv2.medianBlur(img, 3)
 edge = cv2.Canny(img, cv2.CV_8U, 200, 255)
 
 
 plt.figure(figsize=(15, 8))
 
-plt.subplot(2, 2, 1)
+plt.subplot(2, 3, 1)
 plt.imshow(img, cmap='gray')
 plt.title('Original')
 plt.axis("off")
 
-plt.subplot(2, 2, 2)
+plt.subplot(2, 3, 2)
 plt.imshow(laplacian, cmap='gray')
 plt.title('Laplacian')
 plt.axis("off")
        
-plt.subplot(2, 2, 3)
+plt.subplot(2, 3, 3)
 plt.imshow(sobelx, cmap='gray')
 plt.title('Sobel X')
 plt.axis("off")
 
-plt.subplot(2, 2, 4)
+plt.subplot(2, 3, 4)
 plt.imshow(sobely, cmap='gray')
 plt.title('Sobel Y')
 plt.axis("off")
 
-plt.subplot(2, 2, 5)
+plt.subplot(2, 3, 5)
 plt.imshow(edge, cmap='gray')
 plt.title('Edge')
 plt.xticks([]), plt.yticks([])
 
-plt.subplot(2,2,6)
+plt.subplot(2,3,6)
 plt.imshow(blur2, cmap='gray')
 plt.title('medianBlur')
 plt.axis("off")
 
 plt.tight_layout()
 plt.show()
-plt.waitkey()
